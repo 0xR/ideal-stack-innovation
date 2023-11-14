462d908edc838cbb01c6d7dbf9ae2ea0b1ee3aa6
Kubilay Karpat <karpatkubilay@gmail.com>
2023-11-14 11:53:53 +0100

    Insert into db

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,19 +1,24 @@
 "use server";
-import "./db";
 
-import { assertIsNonBlankString } from "@/app/assert-type";
-import { revalidatePath } from "next/cache";
+import {assertIsNonBlankString} from "@/app/assert-type";
+import {revalidatePath} from "next/cache";
+import {db} from "@/app/database/db";
+import {itemsTable} from "@/app/database/schema";
 
 const items: string[] = [];
 
 export async function getItems() {
-  return items;
+    return items;
 }
 
 export async function myAction(formData: FormData) {
-  const item = formData.get("item");
-  assertIsNonBlankString(item);
-  items.push(item);
-  revalidatePath("/");
-  console.log("SERVER!!!!", item);
+    const item = formData.get("item");
+    assertIsNonBlankString(item);
+    items.push(item);
+    revalidatePath("/");
+    console.log("SERVER!!!!", item);
+    const itemRow = await db.insert(itemsTable).values({
+        done: false, name: item
+    }).returning()
+    console.log("itemRow", itemRow);
 }
