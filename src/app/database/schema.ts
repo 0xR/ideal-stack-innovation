36bec6f512ae86b37b943a4a15575c7f71332fa4
Ruben Oostinga <roostinga@xebia.com>
2023-11-14 15:05:22 +0100

    Add tables with relations

--- src/app/database/schema.ts
+++ src/app/database/schema.ts
@@ -1,10 +1,5 @@
-import {
-  boolean,
-  pgTable,
-  serial,
-  uniqueIndex,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { relations } from 'drizzle-orm';
+import { boolean, integer, pgTable, primaryKey, serial, text, varchar, } from "drizzle-orm/pg-core";
 
 export const itemsTable = pgTable(
   "items",
@@ -18,3 +13,40 @@ export const itemsTable = pgTable(
 );
 
 export type Item = typeof itemsTable.$inferSelect;
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: text('name'),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  usersToGroups: many(usersToGroups),
+}));
+
+export const groups = pgTable('groups', {
+  id: serial('id').primaryKey(),
+  name: text('name'),
+});
+
+export const groupsRelations = relations(groups, ({ many }) => ({
+  usersToGroups: many(usersToGroups),
+}));
+
+export const usersToGroups = pgTable('users_to_groups', {
+    userId: integer('user_id').notNull().references(() => users.id),
+    groupId: integer('group_id').notNull().references(() => groups.id),
+  }, (t) => ({
+    pk: primaryKey(t.userId, t.groupId),
+  }),
+);
+
+export const usersToGroupsRelations = relations(usersToGroups, ({ one }) => ({
+  group: one(groups, {
+    fields: [usersToGroups.groupId],
+    references: [groups.id],
+  }),
+  user: one(users, {
+    fields: [usersToGroups.userId],
+    references: [users.id],
+  }),
+}));
