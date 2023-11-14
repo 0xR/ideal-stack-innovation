47c78e6deefd34b6ce5c9ac7d4fff1b58b185b45
Dillon Parfitt <dparfitt@xebia.com>
2023-11-14 13:15:03 +0100

    added caching to db requests

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,22 +1,32 @@
 "use server";
 
-import {assertIsNonBlankString} from "@/app/assert-type";
-import {revalidatePath} from "next/cache";
-import {db} from "@/app/database/db";
-import {itemsTable} from "@/app/database/schema";
+import { assertIsNonBlankString } from "@/app/assert-type";
+import { revalidateTag } from "next/cache";
+import { db } from "@/app/database/db";
+import { itemsTable } from "@/app/database/schema";
+import { unstable_cache as cache } from "next/cache";
 
-
-export async function getItems() {
+export const getItems = cache(
+  () => {
     return db.select().from(itemsTable);
-}
+  },
+  ["items"],
+  {
+    tags: ["items"],
+  }
+);
 
 export async function myAction(formData: FormData) {
-    const item = formData.get("item");
-    assertIsNonBlankString(item);
-    revalidatePath("/");
-    console.log("SERVER!!!!", item);
-    const itemRow = await db.insert(itemsTable).values({
-        done: false, name: item
-    }).returning()
-    console.log("itemRow", itemRow);
+  const item = formData.get("item");
+  assertIsNonBlankString(item);
+  revalidateTag("items");
+  console.log("SERVER!!!!", item);
+  const itemRow = await db
+    .insert(itemsTable)
+    .values({
+      done: false,
+      name: item,
+    })
+    .returning();
+  console.log("itemRow", itemRow);
 }
